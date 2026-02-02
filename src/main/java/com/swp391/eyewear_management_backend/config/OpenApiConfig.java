728f8e17dee6c5157ba34893292d7a56f8ed12a7
Vu Nguyen Duc Huy <soobin205304@gmail.com>
2026-02-02 10:46:02 +0700

    I do not know

--- src/main/java/com/swp391/eyewear_management_backend/config/OpenApiConfig.java
+++ src/main/java/com/swp391/eyewear_management_backend/config/OpenApiConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +18,8 @@ public class OpenApiConfig {
 
         return new OpenAPI()
                 .info(new Info().title("Eyewear API").version("1.0"))
+                .addServersItem(new Server().url("https://localhost:8443").description("HTTPS Server"))
+                .addServersItem(new Server().url("http://localhost:8080").description("HTTP Server (Dev only)"))
                 .addSecurityItem(new SecurityRequirement().addList(schemeName))
                 .components(new Components().addSecuritySchemes(
                         schemeName,
