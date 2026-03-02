46f9eecce6bdd78e3e6ebd8291cfba68c86e0520
Vu Nguyen Duc Huy <soobin205304@gmail.com>
2026-03-02 22:09:53 +0700

    helloworld

--- src/main/java/com/swp391/eyewear_management_backend/dto/request/CartItemRequest.java
+++ src/main/java/com/swp391/eyewear_management_backend/dto/request/CartItemRequest.java
@@ -31,18 +31,28 @@ public class CartItemRequest {
     Double price;
 
     // Prescription fields (optional) - liên kết với bảng Cart_Item_Prescription
+    @DecimalMin(value = "0.0")
     Double rightEyeSph;
+    @DecimalMin(value = "0.0")
     Double rightEyeCyl;
+    @DecimalMin(value = "0.0")
     Integer rightEyeAxis;
+    @DecimalMin(value = "0.0")
     Double rightEyeAdd;
-
+    @DecimalMin(value = "0.0")
     Double leftEyeSph;
+    @DecimalMin(value = "0.0")
     Double leftEyeCyl;
+    @DecimalMin(value = "0.0")
     Integer leftEyeAxis;
+    @DecimalMin(value = "0.0")
     Double leftEyeAdd;
 
     // Pupillary Distance
+    @DecimalMin(value = "30.0")
     Double pd;
+    @DecimalMin(value = "30.0")
     Double pdRight;
+    @DecimalMin(value = "30.0")
     Double pdLeft;
 }
